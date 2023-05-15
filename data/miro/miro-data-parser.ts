00b64c2723c043c451e3934b826627b22723aa00
Christian Scholz <christian.scholz@lise.de>
2023-05-15 17:21:15 +0200

    add miro csv importer

---
+++ data/miro/miro-data-parser.ts
@@ -0,0 +1,14 @@
+import fs from "fs";
+
+const srcDataFilePath = "./data/miro/src/miro-source.csv";
+
+export const getMiroItems = () => {
+  const csvFileContents = fs.readFileSync(srcDataFilePath, "utf-8");
+  const csvRows = csvFileContents.split("\n");
+
+  return csvRows.map((row) => {
+    const [title, area, identifier, content] = row.split(";");
+
+    return { title, area, identifier, content };
+  });
+};
---
+++ data/miro/models/MiroItem.ts
@@ -0,0 +1,6 @@
+export type MiroItem = {
+  title: string;
+  area: string;
+  identifier: string;
+  content: string;
+};
